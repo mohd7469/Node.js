33d49648636c3d94209a7c79cf45c9b3ebda6624
zzzznaz <awais3227443@gmail.com>
2015-05-26 22:04:56 +0500

    added angular get post method

--- routes/index.js
+++ routes/index.js
@@ -5,5 +5,9 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+router.post('/', function(req, res) {
+  console.log(req.body.name);
+  res.send(req.body.name);
+});
 
 module.exports = router;
