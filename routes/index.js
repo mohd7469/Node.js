ad2aca206958f3c623235fcb00087f108f8dfce1
zzzznaz <awais3227443@gmail.com>
2015-05-27 21:50:39 +0500

    post and get request

--- routes/index.js
+++ routes/index.js
@@ -6,8 +6,21 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 router.post('/', function(req, res) {
-  console.log(req.body.name);
+  //console.log(req.body.name);
   res.send(req.body.name);
 });
-
+router.get('/hello', function(req, res) {
+  //console.log(req.body.name);
+  res.send('welcome hello world');
+});
+router.get('/array', function(req, res) {
+  //console.log(req.body.name);
+  var objArr=[{name:'awais'},{name:'bilal'},{name:'noman'}];
+  res.json(objArr);
+});
+router.post('/array', function(req, res) {
+  //console.log(req.body.name);
+  var objArr=[{name:'awais'},{name:'bilal'},{name:'noman'}];
+  res.json(req.body);
+});
 module.exports = router;
